950b01cf8294e70ab3b4296a0c2b45595bbd5280
Colin King <colin.king.96@gmail.com>
2017-01-10 10:20:55 -0500

    Add bucket name argument

--- chapter_04/list_bucket_objects.js
+++ chapter_04/list_bucket_objects.js
@@ -1,9 +1,16 @@
 const AWS = require('aws-sdk');
 const config = require('./config');
 
+if (process.argv.length < 2 || process.argv.length > 3) {
+  console.error('Usage: node list_bucket_objects.js <BUCKET_NAME>');
+  process.exit(1);
+}
+
+const bucketName = (process.argv.length === 2 ? config.BOOK_BUCKET : process.argv[2]);
+
 const s3 = new AWS.S3();
 
-s3.listObjectsV2({ Bucket: config.BOOK_BUCKET }, (err, data) => {
+s3.listObjectsV2({ Bucket: bucketName }, (err, data) => {
   if (err) {
     console.error('Error retrieiving bucket list.');
     console.error(err);
