45c9b7a6298054aa8c2de8a96387797896a25b03
Colin King <colin.king.96@gmail.com>
2017-01-10 10:10:14 -0500

    Add list buckets command

---
+++ chapter_04/list_buckets.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk');
+const config = require('./config');
+
+const s3 = new AWS.S3();
+
+s3.listBuckets((err, data) => {
+  if (err) {
+    console.error('Error retrieiving bucket list.');
+    console.error(err);
+  } else {
+    data.Buckets.forEach((bucket) => {
+      console.log(bucket.Name);
+    });
+  }
+});
