b58bf0626b1671062b03d76b01dcc58a02b2b839
Colin King <colin.king.96@gmail.com>
2017-01-13 15:17:52 -0500

    Added script to post queue messages

---
+++ chapter_06/post_queue.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+const async = require('async');
+const argv = require('minimist')(process.argv.slice(2));
+
+AWS.config.update({ region: 'us-east-1' });
+
+if (argv._.length < 2) {
+  console.error('Usage: node post_queue.js <QUEUE_URL> <ITEM...>');
+  process.exit(1);
+}
+
+const sqs = new AWS.SQS();
+
+const queueUrl = argv._[0];
+const messages = argv._.slice(1);
+
+async.each(messages, (message, cb) => {
+  sqs.sendMessage({
+    QueueUrl: queueUrl,
+    MessageBody: message
+  }, (err) => {
+    if (!err) {
+      console.log(`Posted '${message}' to queue URL ${queueUrl}.`);
+    }
+    cb(err);
+  });
+}, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
