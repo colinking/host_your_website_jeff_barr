1271b699c01df7363cd020635d1f4e7c355bde62
Colin King <colin.king.96@gmail.com>
2017-01-13 23:35:56 -0500

    Restructure config

--- chapter_06/config.js
+++ chapter_06/config.js
@@ -1,4 +1,8 @@
-module.exports.URL_QUEUE = 'c_url';
-module.exports.PARSE_QUEUE = 'c_parse';
-module.exports.IMAGE_QUEUE = 'c_image';
-module.exports.RENDER_QUEUE = 'c_render';
+
+const URL_QUEUE = 'c_url';
+const PARSE_QUEUE = 'c_parse';
+const IMAGE_QUEUE = 'c_image';
+const RENDER_QUEUE = 'c_render';
+const QUEUES = [URL_QUEUE, PARSE_QUEUE, IMAGE_QUEUE, RENDER_QUEUE];
+
+module.exports = { URL_QUEUE, PARSE_QUEUE, IMAGE_QUEUE, RENDER_QUEUE, QUEUES };
