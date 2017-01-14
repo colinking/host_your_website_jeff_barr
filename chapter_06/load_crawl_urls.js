243f86b8177d59647b60db50f2961cec431badf7
Colin King <colin.king.96@gmail.com>
2017-01-13 23:45:46 -0500

    Add script to add URL to queue

---
+++ chapter_06/load_crawl_urls.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const async = require('async');
+const argv = require('minimist')(process.argv.slice(2));
+const config = require('./config');
+const helpers = require('./helpers');
+
+AWS.config.update({ region: 'us-east-1' });
+
+const urls = argv._;
+
+if (urls.length === 0) {
+  console.error('Usage: node load_crawl_urls.js <URL...>');
+  process.exit(1);
+}
+
+const sqs = new AWS.SQS();
+helpers.findQueueURL(sqs, config.URL_QUEUE, (err, queueURL) => {
+  async.each(urls, (url, cb) => {
+    const message = {
+      origin: 'load_crawl_urls.js',
+      data: url,
+      history: [`Posted by load_crawl_urls.js on ${(new Date()).toISOString()}`]
+    };
+    sqs.sendMessage({
+      QueueUrl: queueURL,
+      MessageBody: JSON.stringify(message)
+    }, (err) => {
+      if (!err) {
+        console.log(`Posted '${JSON.stringify(message)}' to '${queueURL}'.`);
+      }
+      cb(err);
+    });
+  }, (err) => {
+    if (err) console.log(err);
+  });
+});
