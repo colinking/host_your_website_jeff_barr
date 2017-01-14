07ea0b930ec7d975cfccb899e4288cfcebdb67e7
Colin King <colin.king.96@gmail.com>
2017-01-14 00:50:28 -0500

    Changed default bucket title

--- chapter_04/config.js
+++ chapter_04/config.js
@@ -1,4 +1,4 @@
-module.exports.DEFAULT_BUCKET = 'colinking-aws-cloud-book';
+module.exports.DEFAULT_BUCKET = 'colinking-aws-cloud';
 
 module.exports.THUMB_BUCKET_SUFFIX = '-thumbnails';
 module.exports.THUMB_SIZE = 200;
