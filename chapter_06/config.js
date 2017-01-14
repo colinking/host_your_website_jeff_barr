ee9845f634a965690289f2639b24e2898aee9352
Colin King <colin.king.96@gmail.com>
2017-01-13 20:26:41 -0500

    Add queue names, findQueueURL, pullMessage

---
+++ chapter_06/config.js
@@ -0,0 +1,4 @@
+module.exports.URL_QUEUE = 'c_url';
+module.exports.PARSE_QUEUE = 'c_parse';
+module.exports.IMAGE_QUEUE = 'c_image';
+module.exports.RENDER_QUEUE = 'c_render';
---
+++ chapter_06/helpers.js
@@ -0,0 +1,31 @@
+const async = require('async');
+
+module.exports.findQueueURL = (sqs, queueName, callback) => {
+  sqs.createQueue({ QueueName: queueName }, (err, data) => {
+    if (err) callback(err);
+    else callback(null, data.QueueUrl);
+  });
+};
+
+module.exports.pullMessage = (sqs, queueURL, callback) => {
+  async.doUntil((cb) => {
+    sqs.receiveMessage({
+      QueueUrl: queueURL,
+      MaxNumberOfMessages: 1
+    }, (err, data) => {
+      if (err) cb(err);
+      else if (data.Messages.length === 0) setTimeout(cb, 1000);
+      else {
+        const message = data.Messages[0];
+        cb(null, {
+          queueURL,
+          timestamp: (new Date()).toISOString(),
+          message,
+          messageBody: message.Body,
+          messageDetail: JSON.parse(message.Body),
+          receiptHandle: message.ReceiptHandle
+        });
+      }
+    });
+  }, message => message, callback);
+};
